0a16229bce9ea93041c8d0abbd424b91f81cee28
yatharth-v <73841421+yatharth-v@users.noreply.github.com>
2021-09-06 20:59:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
+      
+	video = createCapture(VIDEO);
+	video.size (800,400);
+	video.parent('game_console');
+
+	posenet = ml5.poseNet(video,modelloaded);
+    posenet.on('pose',gotposes);
+}
+
+function draw() {
+	game()
+}
+
+function modelloaded(){
+	console.log("model has been loaded successfully");
+}
+function gotposes(results){
+if(results.length>0){
+noseX = results[0].pose.nose.x
+noseY = results[0].pose.nose.y
+}
+}
+
+
+
+
+
+
